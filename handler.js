0af3196e53159c519351e645b21ba1b15e5c6074
sajadweb <radhoosh.user2@gmail.com>
2019-06-09 12:59:58 +0430

    add sample

--- handler.js
+++ handler.js
@@ -10,3 +10,13 @@ module.exports.hello = async (event) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+module.exports.goodby = async (event,context, callback) => {
+  console.log(context)
+  return {
+    statusCode: 200,
+    body:  'goodby',
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
